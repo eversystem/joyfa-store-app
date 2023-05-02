41d5606a23ef129feac742fa243a705d62f6d355
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2023-05-02 11:03:07 +0900

    [update] contract address

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,4 @@
 // export const API_ENDPOINT = 'http://localhost:3000';
 export const API_ENDPOINT = 'https://store-api.joyfa.io:3000';
 export const NFT_COLLECTION_ADDRESS =
-  '0x0cDBC1c80ddB5EBCf10513cEce51e7d922dC4247';
+  '0x6758555D0133F79184B51090132fF45b978Be6d9';
