bad7be5f37bc86a0934e2b7dfed0062b2cf6377c
kojirojoyfa <132132960+kojirojoyfa@users.noreply.github.com>
2023-06-06 01:32:29 +0400

    ハンバーガーメニュー追加
    ChatGPTちゃんと、平手の共同作品

--- src/components/Header/elements/Menu/index.tsx
+++ src/components/Header/elements/Menu/index.tsx
@@ -1,15 +1,29 @@
 import { MenuLink } from './elements/MenuLink';
 import { WalletButton } from './elements/WalletButton';
 import styles from './styles/header-menu.module.css';
+import { useState } from 'react';
 
 export const Menu: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles['menu']}>
-      <MenuLink text="Home" link="/" />
-      <MenuLink text="Store" link="/drops" />
-      <MenuLink text="Create" link="/create" />
-      <MenuLink text="Profile" link="/profile" />
-      <WalletButton />
+      <button
+        className={`${styles['hamburger']} ${isOpen ? styles['open'] : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className={styles['line']}></div>
+        <div className={styles['line']}></div>
+        <div className={styles['line']}></div>
+      </button>
+      <div
+        className={`${styles['menu-links']} ${isOpen ? styles['open'] : ''}`}
+      >
+        <MenuLink text="Home" link="/" />
+        <MenuLink text="Store" link="/drops" />
+        <MenuLink text="Create" link="/create" />
+        <MenuLink text="Profile" link="/profile" />
+        <WalletButton />
+      </div>
     </div>
   );
 };
