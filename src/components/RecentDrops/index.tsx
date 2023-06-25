e1e13e9efa94f153494f5e67edcc7b115f5da708
kojirojoyfa <132132960+kojirojoyfa@users.noreply.github.com>
2023-06-25 04:14:40 +0400

    Topテキスト追加

--- src/components/RecentDrops/index.tsx
+++ src/components/RecentDrops/index.tsx
@@ -4,6 +4,11 @@ import styles from './styles/recent-drops.module.css';
 export const RecentDrops: React.FC = () => {
   return (
     <div className={styles['wrapper']}>
+      <div className={styles['main']}>
+        <div className={styles['main-title']}>
+          The Dawn of the Digital Sneaker Movement
+        </div>
+      </div>
       <div className={styles['title']}>Recent Drops</div>
       <NftList />
     </div>
