377f7bea4b04c27a523a1003123833fc9ccd1f8e
kojirojoyfa <132132960+kojirojoyfa@users.noreply.github.com>
2023-06-26 20:27:41 +0400

    TopページのCSS修正

--- src/components/RecentDrops/index.tsx
+++ src/components/RecentDrops/index.tsx
@@ -1,3 +1,4 @@
+import MainImage from 'src/assets/main-image.png';
 import { NftList } from './elements/NftList';
 import styles from './styles/recent-drops.module.css';
 
@@ -5,8 +6,12 @@ export const RecentDrops: React.FC = () => {
   return (
     <div className={styles['wrapper']}>
       <div className={styles['main']}>
-        <div className={styles['main-title']}>
-          The Dawn of the Digital Sneaker Movement
+        <img className={styles['main-image']} src={MainImage} />
+        <div className={styles['main-box']}>
+          <div className={styles['main-title']}>Red</div>
+          <div className={styles['main-description']}>
+            by Tokyo White International
+          </div>
         </div>
       </div>
       <div className={styles['title']}>Recent Drops</div>
