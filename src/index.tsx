1a2c2da12eb4820833e0d16674e1792539804171
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2023-07-04 14:21:57 +0900

    [fix] profile register

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain="mumbai">
+    <ThirdwebProvider activeChain="ethereum">
       <App />
     </ThirdwebProvider>
   </React.StrictMode>,
