eaf924e7151b86c4ea1b65ec3ac527444cefd518
kojirojoyfa <132132960+kojirojoyfa@users.noreply.github.com>
2023-07-24 04:38:36 +0400

    Muteボタンの追加

--- src/components/NftInfo/elements/NftDetails/elements/Movie/index.tsx
+++ src/components/NftInfo/elements/NftDetails/elements/Movie/index.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import { NftEntity } from 'src/utils/data';
 import styles from './styles/movie.module.css';
 import { resolveIpfsUri } from 'src/utils/resolve-ipfs-uri';
+import MutedIcon from 'src/assets/muted.svg';
+import UnMutedIcon from 'src/assets/unmuted.svg';
 
 export const Movie: React.FC<NftEntity> = (props) => {
+  const [muted, setMuted] = useState(true);
+
+  const handleMute = () => {
+    setMuted(!muted);
+  };
+
   return (
-    <video loop autoPlay className={styles['video']} width={'100%;'}>
-      <source
-        src={resolveIpfsUri(props.metadata.animation_url)}
-        type="video/mp4"
-      />
-    </video>
+    <div className={styles['video-container']}>
+      <video loop autoPlay muted={muted} className={styles['video']}>
+        <source
+          src={resolveIpfsUri(props.metadata.animation_url)}
+          type="video/mp4"
+        />
+      </video>
+      <button onClick={handleMute} className={styles['mute-button']}>
+        {muted
+          ? <img src={MutedIcon} alt="Muted" />
+          : <img src={UnMutedIcon} alt="UnMuted" />
+        }
+      </button>
+    </div>
   );
 };
